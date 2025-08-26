e18de6d5538784e571b2b238e80cbbce0c80d32a
Ahmed Abdelrahim <ahmedabdelrahim0810@gmail.com>
2025-08-27 00:03:32 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
           <Link to="/" className="flex items-center space-x-2">
                          <div className="w-8 h-8 rounded-lg flex items-center justify-center overflow-hidden">
                <img 
-                 src="/pac.jpg" 
+                 src="/pac-removebg-preview.png" 
                  alt="The PAC Logo" 
                  className="w-full h-full object-contain"
                />
